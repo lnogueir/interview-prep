fb63a035ba7c04962dc4cb30544e8a5b2ed0dd0e
Lucas Nogueira <lnogueir@uwaterloo.ca>
2021-04-12 04:42:57 +0000

    docs: added prompt

--- problems/minNumberOfCoinsForChange.py
+++ problems/minNumberOfCoinsForChange.py
@@ -1,3 +1,12 @@
+'''
+Prompt:
+Write a function minNumberOfCoinsForChange(n, denoms) that takes a target number of change 'n'
+and and array of available coins 'denoms', and returns the minimum number of coins needed
+to make up the change 'n'.
+
+Note that you can use the coins in 'denoms' as many times as you'd like.
+'''
+
 def minNumberOfCoinsForChange(n, denoms, cache = {}):
   # print(f'called at n={n}')
   if n in cache:
