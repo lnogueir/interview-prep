effa9aeb8f32cddfbd9565c4faacfe378e8b4832
Lucas Nogueira <lnogueir@uwaterloo.ca>
2021-05-14 23:30:05 -0400

    problem: merge intervals solved

---
+++ problems/mergeIntervals.py
@@ -0,0 +1,38 @@
+'''
+Prompt:
+	Given a collection of closed intervals, merge all overlapping intervals.
+	Ex: mergeIntervals(([[1,4], [2,5], [7,8]]) == [[1,5], [7,8]]
+'''
+
+# Time: O(nlogn), Space: O(n), though one could argue O(1) space
+def mergeIntervals(intervals):
+  mergedIntervals = []
+  intervals.sort(key=lambda item: item[0])
+  currentInterval = []
+  upperBound = -1
+  for interval in intervals:
+    left, right = interval
+    if len(currentInterval) == 0:
+      currentInterval.append(left)
+      upperBound = right
+      continue
+    
+    if left <= upperBound:
+      upperBound = right
+    else:
+      currentInterval.append(upperBound)
+      mergedIntervals.append(currentInterval)
+      currentInterval = [left]
+
+  if len(currentInterval) != 0:
+      currentInterval.append(intervals[-1][1])
+      mergedIntervals.append(currentInterval)
+
+  return mergedIntervals
+  
+t1 = [[2,5], [5, 6], [1,4], [7,8]]
+print(mergeIntervals(t1))
+
+t2 = [[1,4], [2,5], [7,8]]
+print(mergeIntervals(t2))
+
