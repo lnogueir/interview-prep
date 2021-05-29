a9f3cc32dd5e125c877b1de6562d07a402bea2fc
Lucas Nogueira <lnogueir@uwaterloo.ca>
2021-05-29 16:19:09 -0400

    docs: update prompt

--- problems/mergeIntervals.py
+++ problems/mergeIntervals.py
@@ -1,7 +1,7 @@
 '''
 Prompt:
 	Given a collection of closed intervals, merge all overlapping intervals.
-	Ex: mergeIntervals(([[1,4], [2,5], [7,8]]) == [[1,5], [7,8]]
+	Ex: mergeIntervals(([[1,4],[7,8], [2,5], ]) == [[1,5], [7,8]]
 '''
 
 # Time: O(nlogn), Space: O(n), though one could argue O(1) space
